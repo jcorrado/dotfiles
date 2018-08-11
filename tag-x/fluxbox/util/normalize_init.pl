e48d849b5d70abc9b50478477b7d535c98585ff7
Jereme Corrado <jereme@zoion.net>
2018-08-11 10:45:30 -0400

    Added X: Xmodmap, fluxbox, compton, and also conky

---
+++ tag-x/fluxbox/util/normalize_init.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+# My Fluxbox 1.3.5 doesn't have a stable `init' file structure, so
+# trackig in version control is challenging.  This little utility
+# rewrites the init file with a fixed output format.
+
+use warnings;
+use strict;
+
+open my $fh, "< init"
+    or die "couldn't open init for read: $1!\n";
+my $init;
+{
+    local $/;
+    undef $/;
+    $init = <$fh>;
+}
+close $fh;
+
+undef $fh;
+open $fh, "> init"
+    or die "couldn't open init for write: $1!\n";
+
+foreach my $line (sort split /\n/, $init) {
+    my ($k, $v) = $line =~ /^([.\w]+:)\s*(.*)$/;
+    print $fh "$k $v\n";
+}
+close $fh;
