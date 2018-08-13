dd78769eb0c5b91867b5f467ed12c0c987b1ac94
Jereme Corrado <jereme@zoion.net>
2018-08-12 20:40:05 -0400

    Added Leiningen conf for Clojure dev

---
+++ tag-clojure/lein/profiles.clj
@@ -0,0 +1,4 @@
+{:user {:plugins [[lein-try "0.4.3"]
+                  [lein-cljfmt "0.5.7"]
+                  [com.jakemccrary/lein-test-refresh "0.22.0"]
+                  [venantius/ultra "0.5.2"]]}}
