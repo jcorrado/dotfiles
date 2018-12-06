61a0b4e68e259af4addb018d3e364a40df58fe18
Jereme Corrado <jereme@zoion.net>
2018-12-06 07:23:31 -0500

    Began re-org of org conf

--- tag-emacs/emacs.d/config/setup-org-mode.el
+++ tag-emacs/emacs.d/config/setup-org-mode.el
@@ -1,27 +1,37 @@
 (use-package org
-  :ensure t
-  :init (setq org-hide-leading-stars t
-              org-catch-invisible-edits 'show-and-error
-              org-src-window-setup 'current-window
-              org-log-into-drawer 'LOGBOOK
-              org-todo-keywords
-              '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
-                (sequence "WAITING(w)" "HOLD(h)" "|" "CANCELLED(c)"))
-              org-todo-keyword-faces
-              '(("TODO" :foreground "red" :weight bold)
-                ("NEXT" :foreground "cyan" :weight bold)
-                ("WAITING" :foreground "yellow" :weight bold)
-                ("HOLD" :foreground "yellow" :weight bold)
-                ("DONE" :foreground "green" :weight bold)
-                ("CANCELLED" :foreground "green" :weight bold :strike-through t)))
-  :config
-  (add-hook 'org-mode-hook 'flyspell-mode t)
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
-  (add-to-list 'ispell-skip-region-alist '("^#+BEGIN_SRC" . "^#+END_SRC")))
+  :ensure t)
 
 (use-package org-bullets
   :ensure t
   :init (setq org-bullets-bullet-list '("◉" "○" "●" "✚" "✜" "◇" "◆" "✿" "✸" "▸"))
   :config (org-bullets-mode))
+
+;; (global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c a") 'org-agenda)
+;; (global-set-key (kbd "C-c c") 'org-capture)
+;; (global-set-key (kbd "C-c b") 'org-switchb)
+
+;; General Appearance
+(setq org-hide-leading-stars t
+      org-catch-invisible-edits 'show-and-error
+      org-src-window-setup 'current-window
+      org-log-into-drawer 'LOGBOOK)
+
+;; TODOs
+(setq org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+                          (sequence "WAITING(w)" "HOLD(h)" "|" "CANCELLED(c)"))
+      org-todo-keyword-faces '(("TODO" :foreground "red" :weight bold)
+                               ("NEXT" :foreground "cyan" :weight bold)
+                               ("WAITING" :foreground "yellow" :weight bold)
+                               ("HOLD" :foreground "yellow" :weight bold)
+                               ("DONE" :foreground "green" :weight bold)
+                               ("CANCELLED" :foreground "green" :weight bold :strike-through t)))
+
+(setq org-agenda-files '("~/org/auaap.org"
+                         "~/org/notes.org"
+                         "~/org/todo.org"))
+
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+(add-to-list 'ispell-skip-region-alist '("^#+BEGIN_SRC" . "^#+END_SRC"))
 
 (provide 'setup-org-mode)
