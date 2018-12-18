4e719b3d53bc5c8584890193277e2ff31da226c5
Jereme Corrado <jereme@zoion.net>
2018-12-18 17:45:24 -0500

    Started digging into org-mode setup

--- tag-emacs/emacs.d/config/setup-org-mode.el
+++ tag-emacs/emacs.d/config/setup-org-mode.el
@@ -1,32 +1,30 @@
 (use-package org
-  :ensure t)
+  :ensure t
+  :diminish org-indent-mode
+  :init (setq org-startup-indented t
+              org-src-window-setup 'current-window
+              org-log-into-drawer 'LOGBOOK
+              org-catch-invisible-edits 'show-and-error))
 
-;; (global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c a") 'org-agenda)
+;; (global-set-key (kbd "C-c l") 'org-store-link)
 ;; (global-set-key (kbd "C-c c") 'org-capture)
 ;; (global-set-key (kbd "C-c b") 'org-switchb)
 
 (use-package org-bullets
   :ensure t
-  :init (setq org-bullets-bullet-list '("●" "○" "◉" "✚" "✜" "◇" "◆" "✿" "✸" "▸"))
+  :init (setq org-bullets-bullet-list '("●" "◉" "○"))
   :config (org-bullets-mode))
 
-(use-package org-gcal
-  :ensure t)
+(use-package org-gcal :ensure t)
 
 ;;
 ;; Agenda
 ;;
-(add-hook 'org-agenda-mode-hook (lambda () (org-gcal-fetch)))
+;;(add-hook 'org-agenda-mode-hook (lambda () (org-gcal-fetch)))
 ;;(add-hook 'org-capture-after-finalize-hook (lambda () (org-gcal-sync)))
 
 (setq org-agenda-span 1)
-
-;; General Appearance
-(setq org-hide-leading-stars t
-      org-catch-invisible-edits 'show-and-error
-      org-src-window-setup 'current-window
-      org-log-into-drawer 'LOGBOOK)
 
 ;; TODOs
 (setq org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "IN-PROGRESS(i)" "|" "DONE(d)")
@@ -38,11 +36,26 @@
                                ("DONE" :foreground "dim gray")
                                ("CANCELED" :foreground "dim gray" :strike-through t)))
 
-(setq org-agenda-files '("~/org/gcal/personal"
+;; Give some thought to this as my process evolves
+;; (setq org-agenda-todo-list-sublevels nil)
+
+(setq org-agenda-custom-commands
+      '(("p" . "Personal agenda views")
+        ("pt" "Personal Upcoming Tasks" tags-todo "personal+TODO=\"TODO\"|personal+TODO=\"NEXT\"")
+        ("pn" "Personal NEXT" tags-todo "personal+TODO=\"NEXT\"")
+
+        ("b" . "Birchbox agenda views")
+        ("bt" "Birchbox Upcoming Tasks" tags-todo "birchbox+TODO=\"TODO\"|birchbox+TODO=\"NEXT\"")
+        ("bn" "Birchbox NEXT" tags-todo "birchbox+TODO=\"NEXT\"")
+
+        ("x" "Agenda + task view"
+         ((agenda)
+          (todo "NEXT")))))
+
+(setq org-agenda-files '("~/org/todo"
+                         "~/org/gcal/personal"
                          "~/org/gcal/birchbox"
-                         "~/org/auaap.org"
-                         "~/org/notes.org"
-                         "~/org/todo.org"))
+                         "~/org/notes.org"))
 
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 (add-to-list 'ispell-skip-region-alist '("^#+BEGIN_SRC" . "^#+END_SRC"))
