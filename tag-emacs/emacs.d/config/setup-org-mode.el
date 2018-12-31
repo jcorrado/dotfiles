3b78f34cad7df48e1461bbcb046b71173ac20248
Jereme Corrado <jereme@zoion.net>
2018-12-31 11:30:53 -0500

    Org: config layout reorg

--- tag-emacs/emacs.d/config/setup-org-mode.el
+++ tag-emacs/emacs.d/config/setup-org-mode.el
@@ -15,7 +15,6 @@
               org-tags-column -100)
   :config (progn
             (global-set-key (kbd "C-c a") 'org-agenda)
-            ;; (global-set-key (kbd "C-c c") 'hydra-org-capture/body)
             (global-set-key (kbd "C-c c") 'org-capture)
             (add-to-list 'ispell-skip-region-alist '("^#+BEGIN_SRC" . "^#+END_SRC"))))
 
@@ -57,11 +56,56 @@
 
 
 ;;
+;; Capture
+;;
+(setq org-capture-templates
+      '(("p" "Personal Task" entry (file+headline my/org-inbox "Personal")
+         "* TODO %?\n")
+        ("e" "Errands" entry (file my/org-errands)
+         "* TODO %?\n")
+        ("w" "Work Tasks")
+        ("wb" "Birchbox Task" entry (file+headline my/org-inbox "Birchbox")
+         "* TODO %?\n")
+        ("we" "Empatico Task" entry (file+headline my/org-inbox "Empatico")
+         "* TODO %?\n")
+        ("m" "Personal Mail Followup Task" entry (file+headline my/org-personal-todo  "Tasks")
+         "* NEXT [[%:link][%:description]] :@email:%i\n%?\n")
+        ("n" "Birchbox Mail Followup Task" entry (file+headline my/org-birchbox-todo  "Tasks")
+         "* NEXT [[%:link][%:description]] :@email:%i\n%?\n")))
+
+(setq org-refile-targets '((my/org-todo-files . (:maxlevel . 3)))
+      org-refile-use-outline-path 'file
+      org-outline-path-complete-in-steps nil)
+
+
+;;
+;; Tasks
+;;
+(setq org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "IN-PROGRESS(i!/!)" "|" "DONE(d!/!)")
+                          (sequence "WAITING(w@/!)" "|" "CANCELED(c@/!)")
+                          (sequence "HABIT(h)" "|" "DONE(d!)" "CANCELED(c@/!)"))
+      org-todo-keyword-faces '(("TODO" :foreground "orange red" :weight bold)
+                               ("NEXT" :foreground "lawn green" :weight bold)
+                               ("IN-PROGRESS" :foreground "navy" :background "sky blue" :weight bold)
+                               ("WAITING" :foreground "yellow" :weight bold)
+                               ("DONE" :foreground "dim gray")
+                               ("CANCELED" :foreground "dim gray" :strike-through t)
+                               ("HABIT" :foreground "light blue" :weight bold))
+      org-log-into-drawer 'LOGBOOK
+      org-log-done-with-time t
+      org-closed-keep-when-no-todo t
+      org-treat-insert-todo-heading-as-state-change nil)
+
+;; Habits
+(setq org-habit-graph-column 58
+      org-habit-preceding-days 21
+      org-habit-following-days 7
+      org-habit-show-habits-only-for-today t)
+
+
+;;
 ;; Agenda
 ;;
-;;(add-hook 'org-agenda-mode-hook (lambda () (org-gcal-fetch)))
-;;(add-hook 'org-capture-after-finalize-hook (lambda () (org-gcal-sync)))
-
 (setq org-agenda-tags-todo-honor-ignore-options t
       org-agenda-todo-list-sublevels nil
       org-agenda-span 1
@@ -100,66 +144,6 @@
                  (org-agenda-todo-ignore-scheduled 'all))))
          ((org-agenda-tag-filter-preset '("-shared"))))
         ))
-
-
-;;
-;; Capture
-;;
-(setq org-capture-templates
-      '(("p" "Personal Task" entry (file+headline my/org-inbox "Personal")
-         "* TODO %?\n")
-        ("e" "Errands" entry (file my/org-errands)
-         "* TODO %?\n")
-        ("w" "Work Tasks")
-        ("wb" "Birchbox Task" entry (file+headline my/org-inbox "Birchbox")
-         "* TODO %?\n")
-        ("we" "Empatico Task" entry (file+headline my/org-inbox "Empatico")
-         "* TODO %?\n")
-        ("m" "Personal Mail Followup Task" entry (file+headline my/org-personal-todo  "Tasks")
-         "* NEXT [[%:link][%:description]] :@email:%i\n%?\n")
-        ("n" "Birchbox Mail Followup Task" entry (file+headline my/org-birchbox-todo  "Tasks")
-         "* NEXT [[%:link][%:description]] :@email:%i\n%?\n")))
-
-(setq org-refile-targets '((my/org-todo-files . (:maxlevel . 3)))
-      org-refile-use-outline-path 'file
-      org-outline-path-complete-in-steps nil)
-
-;; ;; Les intrusive than the standard org-capture pop-up, but maybe it doesn't matter
-;; (defhydra hydra-org-capture (:hint nil)
-;;   "
-;; Org Capture:
-;; ^^[_p_]ersonal task  ^^[_b_]irchbox task  ^^[_e_]mpatico task
-;; "
-;;   ("p" (org-capture nil "p"))
-;;   ("b" (org-capture nil "b"))
-;;   ("e" (org-capture nil "e"))
-;;   ("q" nil :color blue)
-;;   )
-
-
-;;
-;; Tasks
-;;
-(setq org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "IN-PROGRESS(i!/!)" "|" "DONE(d!/!)")
-                          (sequence "WAITING(w@/!)" "|" "CANCELED(c@/!)")
-                          (sequence "HABIT(h)" "|" "DONE(d!)" "CANCELED(c@/!)"))
-      org-todo-keyword-faces '(("TODO" :foreground "orange red" :weight bold)
-                               ("NEXT" :foreground "lawn green" :weight bold)
-                               ("IN-PROGRESS" :foreground "navy" :background "sky blue" :weight bold)
-                               ("WAITING" :foreground "yellow" :weight bold)
-                               ("DONE" :foreground "dim gray")
-                               ("CANCELED" :foreground "dim gray" :strike-through t)
-                               ("HABIT" :foreground "light blue" :weight bold))
-      org-log-into-drawer 'LOGBOOK
-      org-log-done-with-time t
-      org-closed-keep-when-no-todo t
-      org-treat-insert-todo-heading-as-state-change nil)
-
-;; Habits
-(setq org-habit-graph-column 58
-      org-habit-preceding-days 21
-      org-habit-following-days 7
-      org-habit-show-habits-only-for-today t)
 
 
 ;;
