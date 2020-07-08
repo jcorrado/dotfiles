4ae668627fe292ea3ace575d498a86789b7ebd24
Jereme Corrado <jereme@teammobot.com>
2020-07-08 15:23:41 -0400

    Clojure: try lein-gorilla

--- tag-clojure/lein/profiles.clj
+++ tag-clojure/lein/profiles.clj
@@ -1,3 +1,4 @@
 {:user {:plugins [[lein-try "0.4.3"]
-                  [lein-cljfmt "0.5.7"]]}}
+                  [lein-cljfmt "0.5.7"]
+                  [org.clojars.benfb/lein-gorilla "0.7.0"]]}}
 {:repl {:plugins [[cider/cider-nrepl "0.21.1"]]}}
